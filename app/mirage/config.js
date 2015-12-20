f2519062a6fd8d091679f4158352593e7580992a
Robert Kaufman <rob.m.kaufman@gmail.com>
2015-12-20 15:07:36 -0500

    Fix pretend server response to send table_id, set up hand route

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -5,7 +5,7 @@ export default function() {
         type: 'hands',
         id: "3180680492",
         attributes: {
-          tableId: "10239001",
+          table_id: "10239001",
           time: "2015-11-26 20:22:07"
         }
       }]
@@ -18,7 +18,7 @@ export default function() {
         type: 'hands',
         id: "3180680492",
         attributes: {
-          tableId: "10239001",
+          table_id: "10239001",
           time: "2015-11-26 20:22:07"
         }
       }
